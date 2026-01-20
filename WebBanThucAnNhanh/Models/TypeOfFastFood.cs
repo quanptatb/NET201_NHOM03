065209d2587ebf0fab8f8205b1db6a265901dadb
quanptatb <quanptatb01758@gmail.com>
2026-01-20 17:27:45 +0700

    model typeoffastfood

---
+++ WebBanThucAnNhanh/Models/TypeOfFastFood.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBanThucAnNhanh.Models
+{
+    public class TypeOfFastFood
+    {
+        public int IdTypeOfFastFood { get; set; }
+        public string NameTypeOfFastFood { get; set; }
+    }
+}
