362a1f081c2e69b96bd7c062f75f52e40ba543c4
quanptatb <quanptatb01758@gmail.com>
2026-01-20 16:45:07 +0700

    create project

---
+++ WebBanThucAnNhanh/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebBanThucAnNhanh.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
