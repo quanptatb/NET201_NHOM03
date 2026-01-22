17c53d89e8ef9ddec1aad52eb66f6a94be52c587
Pham Tran An An <anptatb01817@gmail.com>
2026-01-22 09:39:34 +0700

    Register

--- WebBanThucAnNhanh/Controllers/HomeController.cs
+++ WebBanThucAnNhanh/Controllers/HomeController.cs
@@ -1,24 +1,49 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebBanThucAnNhanh.Data;
 using WebBanThucAnNhanh.Models;
 
-namespace WebBanThucAnNhanh.Controllers;
-
-public class HomeController : Controller
+namespace WebBanThucAnNhanh.Controllers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View();
-    }
+        private readonly AppDbContext _context;
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(string searchString, int? categoryId)
+        {
+            var fastFoods = _context.FastFoods.Include(f => f.TypeOfFastFood).AsQueryable();
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                fastFoods = fastFoods.Where(s => s.NameFastFood.Contains(searchString));
+            }
+
+            if (categoryId.HasValue)
+            {
+                fastFoods = fastFoods.Where(x => x.IdTypeOfFastFood == categoryId);
+            }
+
+            ViewBag.Categories = await _context.TypeOfFastFoods.ToListAsync();
+            
+            ViewBag.CurrentFilter = searchString;
+
+            return View(await fastFoods.ToListAsync());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
